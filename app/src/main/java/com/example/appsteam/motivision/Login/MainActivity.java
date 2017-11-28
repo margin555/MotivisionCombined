0c113991a7d255572f15ff3f8f45ed5cbed9b1f8
margin ks <margin555@gmail.com>
2017-11-28 15:10:37 +0530

    login stage complete

--- app/src/main/java/com/example/appsteam/motivision/Login/MainActivity.java
+++ app/src/main/java/com/example/appsteam/motivision/Login/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.appsteam.motivision.Login;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -20,25 +21,26 @@ import com.example.appsteam.motivision.common.Utility;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText username,password;
+    private EditText username, password,f_Answer;
     private Button login;
-    private  TextView  register,forgot_password;
-  private   Utility utility;
-    private String  musername,mpassword;
+    private TextView register, forgot_password,security_frgt;
+    private Utility utility;
+    private String musername, mpassword;
     private MotivisionDbManager mdbmanager;
+    public static final String MOTIVISION_PREFS_NAME = "MyPrefs";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         register=(TextView)findViewById(R.id.textView4);
-        forgot_password=(TextView)findViewById(R.id.forgotpswd);
-       login=(Button)findViewById(R.id.login_button);
-       username=(EditText)findViewById(R.id.username);
-       password=(EditText)findViewById(R.id.password);
-        mdbmanager =new MotivisionDbManager(getApplicationContext());
-
+        register = (TextView) findViewById(R.id.textView4);
+        forgot_password = (TextView) findViewById(R.id.forgotpswd);
+        login = (Button) findViewById(R.id.login_button);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        mdbmanager = new MotivisionDbManager(getApplicationContext());
+        final SharedPreferences.Editor editor = getSharedPreferences(MOTIVISION_PREFS_NAME, MODE_PRIVATE).edit();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 if (musername.equals("") || mpassword.equals("")) {
                     utility.setToast(getApplicationContext(), getResources().getString(R.string.Enter_All));
                 } else if (mpassword.length() > 8) {
-                    Cursor cursor = mdbmanager.getDataUsernamePassword(musername,mpassword);
+                    Cursor cursor = mdbmanager.getDataUsernamePassword(musername, mpassword);
                     if (cursor.getCount() == 0) {
                         utility.setToast(getApplicationContext(), getResources().getString(R.string.Invalid_usrname_passwd));
                         username.setText("");
@@ -57,21 +59,21 @@ public class MainActivity extends AppCompatActivity {
                         String mGetDb_Username = cursor.getString(cursor.getColumnIndex(mdbmanager.MOTIVISION_REGISTRATION_COLUMN_USERNAME));
                         String mGetDb_Passwd = cursor.getString(cursor.getColumnIndex(mdbmanager.MOTIVISION_REGISTRATION_COLUMN_PASSWORD));
                         if (musername.equals(mGetDb_Username) && mpassword.equals(mGetDb_Passwd)) {
-
                             Intent cir_prgrs = new Intent(MainActivity.this, Progress.class);
                             startActivity(cir_prgrs);
                         }
 
-            }}
-            else{
-                    utility.setToast(getApplicationContext(),getResources().getString(R.string.passwd_length));
-                }}
+                    }
+                } else {
+                    utility.setToast(getApplicationContext(), getResources().getString(R.string.passwd_length));
+                }
+            }
         });
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent reg_intent=new Intent(MainActivity.this,Registration.class);
+                Intent reg_intent = new Intent(MainActivity.this, Registration.class);
                 startActivity(reg_intent);
             }
         });
@@ -80,33 +82,54 @@ public class MainActivity extends AppCompatActivity {
         forgot_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
-                View mView = getLayoutInflater().inflate(R.layout.activity_forget_password, null);
-                 musername=username.getText().toString();
-Cursor cursor=mdbmanager.getRegistrationFullData(musername);
-if(musername.equals())
-                TextView security_frgt = (TextView) mView.findViewById(R.id.security_frgt);
-                final EditText mAnswer = (EditText) mView.findViewById(R.id.Answer);
-                mBuilder.setPositiveButton("Request", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+                musername = username.getText().toString();
+                final Cursor cursor = mdbmanager.getRegistrationFullData(musername);
+                if (musername.equals("")) {
+                    utility.setToast(getApplicationContext(), getResources().getString(R.string.Enter_All));
 
-               Intent reset_intent=new Intent(MainActivity.this,ResetPassword.class);
-               startActivity(reset_intent);
-                    }
-                });
+                } else if (cursor.getCount() == 0) {
+                    utility.setToast(getApplicationContext(), getResources().getString(R.string.Enter_a_valid_Username));
+                } else if (cursor.getCount() != 0) {
+                    cursor.moveToFirst();
+                    AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                    View mView = getLayoutInflater().inflate(R.layout.activity_forget_password, null);
+                    security_frgt = (TextView)mView.findViewById(R.id.security_frgt);
+                    String mquestion;
+                    mquestion = cursor.getString(cursor.getColumnIndex(MotivisionDbManager.MOTIVISION_REGISTRATION_COLUMN_SECURITY_QUESTION));
+                    security_frgt.setText(mquestion);
 
-                mBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-               });
-                mBuilder.setView(mView);
+                    f_Answer = (EditText) mView.findViewById(R.id.Answer);
+                    mBuilder.setPositiveButton("Request", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            String m_answer, db_answer;
+
+                            m_answer = f_Answer.getText().toString();
+                            db_answer = cursor.getString(cursor.getColumnIndex(MotivisionDbManager.MOTIVISION_REGISTRATION_COLUMN_ANSWER));
+                            if (m_answer.equals(db_answer)) {
+                                editor.putString("key_username",musername);
+                                editor.commit();
+                                Intent reset_intent = new Intent(MainActivity.this, ResetPassword.class);
+                                startActivity(reset_intent);
+                            } else {
+                                utility.setToast(getApplicationContext(), getResources().getString(R.string.wrong_answer));
+                            }
+                        }
+                    });
+
+                    mBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    mBuilder.setView(mView);
                     final AlertDialog dialog = mBuilder.create();
-                dialog.show();
+                    dialog.show();
 
 
+                }
             }
         });
 
--- app/src/main/java/com/example/appsteam/motivision/Login/ResetPassword.java
+++ app/src/main/java/com/example/appsteam/motivision/Login/ResetPassword.java
@@ -1,24 +1,38 @@
 package com.example.appsteam.motivision.Login;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.appsteam.motivision.R;
+import com.example.appsteam.motivision.common.MotivisionDbManager;
+import com.example.appsteam.motivision.common.Utility;
+
+import static com.example.appsteam.motivision.Login.MainActivity.MOTIVISION_PREFS_NAME;
 
 public class ResetPassword extends AppCompatActivity {
+private EditText reset_passwd,resert_cnfrm_passwd;
+private String m_reset_passwd,m_confrm_password;
+private Utility utility;
+private MotivisionDbManager motivisionDbManager;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+    prefs = getSharedPreferences(MOTIVISION_PREFS_NAME, MODE_PRIVATE);
         Button reset=(Button)findViewById(R.id.reset);
         TextView reset_login=(TextView)findViewById(R.id.reset_login);
-        
+        reset_passwd=(EditText)findViewById(R.id.reset_passwd);
+        resert_cnfrm_passwd=(EditText)findViewById(R.id.reset_cnfrmpswd);
+        motivisionDbManager=new MotivisionDbManager(getApplicationContext());
         reset_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,7 +44,17 @@ public class ResetPassword extends AppCompatActivity {
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ResetPassword.this, "Success! Your Password has been changed!", Toast.LENGTH_SHORT).show();
+                m_reset_passwd=reset_passwd.getText().toString();
+                m_confrm_password=resert_cnfrm_passwd.getText().toString();
+                String frgt_username;
+                frgt_username=prefs.getString("key_username","");
+                if(m_confrm_password.equals(m_reset_passwd)) {
+                    motivisionDbManager.updatePin(frgt_username,m_confrm_password);
+                    Toast.makeText(ResetPassword.this, "Success! Your Password has been changed!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    utility.setToast(getApplicationContext(),getResources().getString(R.string.Passwords_not_matching));
+                }
             }
         });
         
